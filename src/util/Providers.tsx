56a00f838c74883d18799b280cdd732fc5e1ece2
Ellis Yoo <ellisyoo@elliss-mbp.lan>
2024-01-17 17:03:06 -0800

    create provider

---
+++ src/util/Providers.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { ReactNode, useState } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const Providers = ({children} : {children: ReactNode}) => {
+    const [queryClient] = useState(() => new QueryClient())
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools initialIsOpen={false} />
+            {children}
+        </QueryClientProvider>
+    )
+}
+
+export default Providers
